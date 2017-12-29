311bea2080dda15a4d7d29fce22aa588277a5e82
Chris Zuschlag <chris.zuschlag.public@gmail.com>
2017-12-29 11:37:19 -0700

    Minor Updates, A Few More Method Shells
    The User, Workout, and Exercise objects are turning out to be the activity hubs of the application. This makes a lot of sense, of course.

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,11 +2,117 @@
  * 
  */
 package main.java;
+import java.time;
 
 /**
  * @author Christopher
  *
  */
 public class User {
+	
+	int userID;
+	String loginID;
+	String password;
+	String firstName;
+	String lastName;
+	float userWeight;
+	
+	Statistic[] getUserStatisticList()
+	{
+		
+		
+	}
+	
+	int getUserStatisticValue(String statisticName)
+	{
+		
+		
+	}
+	
+	int getUserStatisticValue(String statisticName, OffsetDateTime asOf)
+	{
+		
+		
+	}
+	
+	Location[] getLocationList()
+	{
+		
+		
+	}
+	
+	Workout[] getMyWorkouts()
+	{
+		
+		
+	}
+	
+	void SetWorkout(int baseWorkout)
+	{
+		
+		
+		
+	}
+	
+	Exercise[] getMyExercises()
+	{
+		
+		
+	}
 
+	Schedule getMySchedule()
+	{
+		
+		
+	}
+
+	void setWeight()
+	{
+		
+		
+	}
+	
+	void removeUser()
+	{
+		
+		
+	}
+	
+	boolean isValidPassword()
+	{
+	
+		
+	}
+	
+	void createNewUserStatisic(String statisticName)
+	{
+		
+		
+	}
+	
+	void setUserStatisicValue(String statisticName, OffsetDateTime asOf)
+	{
+		
+		
+	}
+	
+	
+	WorkoutLocation[] getWorkoutLocations()
+	{
+		
+		
+	}
+	
+	void createWorkoutLocation(String locationName)
+	{
+		WorkoutLocation addedLocation;
+		
+		if
+		{
+			addedLocation = new WorkoutLocation(locationName);
+			addedLocation.userid = userID;
+		}
+		else
+			throw DuplicateValueException;
+	}
 }
